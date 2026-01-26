cd47d9785e4fe4eb9503d4b7527b9aa53da67e2a
andrejacobspy <andrejacobs365@gmail.com>
2026-01-26 15:45:51 -0700

    making skill pills look correct in light mode as well

--- src/app/components/SkillPill.tsx
+++ src/app/components/SkillPill.tsx
@@ -7,7 +7,7 @@ interface SkillPillProps {
 
 const SkillPill: FunctionComponent<SkillPillProps> = ({ skill, pixelWidth }) => {
   return (
-    <div className="relative bg-neutral-800 h-15 w-90 p-4 rounded-lg m-4 align-middle">
+    <div className="relative dark:bg-neutral-800 bg-neutral-200 h-15 w-90 p-4 rounded-lg m-4 align-middle">
       <h2 className="text-md">{skill}</h2>
       <div className="absolute top-6 left-40 h-2 w-40 bg-neutral-700 rounded-md z-10"/>
       <div className={`absolute top-6 left-40 h-2 bg-blue-500 rounded-md z-50`} style={{ width: `${pixelWidth}px` }}/>
